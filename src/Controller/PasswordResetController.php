e80e8493bce16620baf955ddf1a449aca8f10e2f
ghassen <ghassendhaoui.eng@gmail.com>
2025-04-23 01:55:05 +0100

    send reset password

--- src/Controller/PasswordResetController.php
+++ src/Controller/PasswordResetController.php
@@ -2,37 +2,51 @@
 
 namespace App\Controller;
 
-use Psr\Log\LoggerInterface;
+use App\Entity\PasswordResetToken;
+use App\Entity\User;
+use App\Form\PasswordResetType;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\Mailer\Exception\TransportExceptionInterface;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PasswordResetController extends AbstractController
 {
-    #[Route('/password/reset', name: 'app_password_reset')]
-    public function index(): Response
-    {
+    #[Route('/password/reset/{token}', name: 'app_password_reset')]
+    public function index(
+        EntityManagerInterface $entityManager,
+        string $token,
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher
+    ): Response {
+        $tokenEntity = $entityManager->getRepository(PasswordResetToken::class)
+            ->findOneBy(["token" => $token]);
+
+        if (!$tokenEntity || $tokenEntity->getExpiresAt() < new \DateTimeImmutable()) {
+            $this->addFlash('warning', 'Invalid or expired token');
+            return $this->redirectToRoute('app_login');
+        }
+
+        $user = $tokenEntity->getUser();
+        $form = $this->createForm(PasswordResetType::class, $user); // Pass the actual user
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            // Get the password from plainPassword field
+            $user->setPassword(
+                $passwordHasher->hashPassword($user, $form->get('plainPassword')->getData()) // Changed from 'password')
+            );
+            $entityManager->remove($tokenEntity);
+            $entityManager->flush();
+
+            $this->addFlash('success', 'Password updated successfully');
+            return $this->redirectToRoute('app_login');
+        }
+
         return $this->render('password_reset/index.html.twig', [
-            'controller_name' => 'PasswordResetController',
+            'form' => $form->createView(),
         ]);
-    }
-
-    /**
-     * @throws TransportExceptionInterface
-     */
-    #[Route('/test-email')]
-    public function testEmail(MailerInterface $mailer): Response
-    {
-        $email = (new Email())
-            ->from('test@yoursportsapp.com')
-            ->to('test@example.com')
-            ->subject('MailHog Test')
-            ->text('This should appear in MailHog!');
-
-        $mailer->send($email);
-        return new Response('Email sent. Check MailHog at http://localhost:8025');
     }
 }
